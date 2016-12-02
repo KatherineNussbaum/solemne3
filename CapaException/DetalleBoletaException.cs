22c8e41728162e5dd993ece353c3fcff99e0ac82
PelaoVergara <rodrigo.vergaraa@gmail.com>
2016-12-02 20:31:57 -0300

    Capa Exceptions
    Capa Exceptions

---
+++ CapaException/DetalleBoletaException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaException
+{
+    class DetalleBoletaException : Exception
+    {
+        public DetalleBoletaException(string mensaje) : base(mensaje)
+        {
+
+        }
+    }
+}
---
+++ CapaException/ProductoException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaException
+{
+    class ProductoException : Exception
+    {
+        public ProductoException(string mensaje) : base(mensaje)
+        {
+
+        }
+    }
+}
