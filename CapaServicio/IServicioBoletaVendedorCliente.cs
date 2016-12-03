fe6d97c0e004ddf57fcfddc50357abbebf146d92
AlteraMobile <katherine@altera.cl>
2016-12-03 01:19:58 -0300

    ServicioKatherine

---
+++ CapaServicio/IServicioBoletaVendedorCliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+using CapaDato;
+using CapaEntidad;
+using CapaInterface;
+using CapaNegocio;
+
+namespace CapaServicio
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicioBoletaVendedorCliente" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicioBoletaVendedorCliente
+    {
+        [OperationContract]
+        IList<BoletaVendedorCliente> ListarBoletaVendedorCliente(decimal idVendedor);
+    }
+}
---
+++ CapaServicio/ServicioBoletaVendedorCliente.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+using CapaDato;
+using CapaEntidad;
+using CapaNegocio;
+using CapaInterface;
+
+namespace CapaServicio
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioBoletaVendedorCliente" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ServicioBoletaVendedorCliente.svc o ServicioBoletaVendedorCliente.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ServicioBoletaVendedorCliente : IServicioBoletaVendedorCliente
+    {
+        private IBoletaVendedorClienteBO _boletaVendedorClienteBO;
+        
+        public IList<BoletaVendedorCliente> ListarBoletaVendedorCliente(decimal idVendedor)
+        {
+            return this._boletaVendedorClienteBO.ListarBoletaVendedorCliente(idVendedor);
+        }
+    }
+}
